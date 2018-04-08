f997f1307f242703762572068ea3cc34cc2b2135
Your Name <you@example.com>
2018-04-08 11:13:31 +0400

    test

---
+++ lesson2/script.js
@@ -0,0 +1,12 @@
+var obj = {
+    "first_name": "Vardan",
+    "last_name": "Hovsepyan",
+    "age": 13,
+    "tumo_student": true,
+    sayHello() {
+        console.log("this.first_name");
+    }
+}
+console.log(obj);
+obj.sayHello();
+
---
+++ public/class.eatgrass.js
@@ -0,0 +1,25 @@
+
+
+class GrassEater extends LivingCreature {
+    constructor(x, y, index){
+        super(x, y, index);
+        this.tariq = 0;
+    }
+   getNewCoordinates() {
+       this.directions = [
+           [this.x - 1, this.y - 1],
+           [this.x, this.y - 1],
+           [this.x + 1, this.y - 1],
+           [this.x - 1, this.y],
+           [this.x + 1, this.y],
+           [this.x - 1, this.y + 1],
+           [this.x, this.y + 1],
+           [this.x + 1, this.y + 1]
+       ];
+   }
+   chooseCell(character) {
+       this.getNewCoordinates();
+       return super.chooseCell(character);
+   }
+   
+}
